7b73f9caedefb364d39d1ad661eb13c9e9fd7191
imohammad24 <s11941357@stu.najah.edu>
2024-03-17 13:31:26 +0200

    initialize project (routes,controllers)

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan")
+
+// bring in routes
+const postRoutes = require('./routes/post')
+
+// middleware
+app.use(morgan("dev"));
+
+app.use("/", postRoutes);
+
+const port = 8080
+app.listen(port,()=>{
+    console.log('A Node Js API is listening on port: '+port)
+});
